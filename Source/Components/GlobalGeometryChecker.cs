a26e5e3cfa4f012e6caa3caaf4a61488610f35ce
SuiMachine <SuiMachine@users.noreply.github.com>
2022-07-13 12:36:50 +0200

    ggadjfgadjgb

---
+++ Source/Components/GlobalGeometryChecker.cs
@@ -0,0 +1,156 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+using static MelonLoader.MelonLogger;
+
+namespace SuisHack.Components
+{
+	[RegisterTypeInIl2Cpp]
+	public class GlobalGeometryChecker : MonoBehaviour
+	{
+		const string OpenWorldPrefix = "OpenWorld_";
+		const string Terrain = "Terrain_Mesh";
+
+		static HashSet<string> yeets = new HashSet<string>();
+
+
+		public GlobalGeometryChecker(IntPtr ptr) : base(ptr) { }
+		private object CheckerCoroutine;
+
+		private void OnEnable()
+		{
+			if (CheckerCoroutine != null)
+				MelonCoroutines.Stop(CheckerCoroutine);
+			CheckerCoroutine = MelonCoroutines.Start(Checker());
+		}
+
+		private System.Collections.IEnumerator Checker()
+		{
+			while (true)
+			{
+				var rootObject = this.gameObject.scene.GetRootGameObjects();
+				yield return null;
+
+				for (int i = 0; i < rootObject.Length; i++)
+				{
+					var obj = rootObject[i];
+					if (obj != null)
+					{
+						if (obj.name.StartsWith(OpenWorldPrefix))
+						{
+							var components = obj.GetComponentsInChildren<ElectricPoleModel>();
+							yield return null;
+							for (int j = 0; j < components.Length; j++)
+							{
+								var component = components[j];
+
+								//since we wait after adding, this can become null
+								if (component != null)
+								{
+									if (component.GetComponent<WireRendererCorrection>() == null)
+									{
+										component.gameObject.AddComponent<WireRendererCorrection>();
+										yield return null;
+									}
+								}
+							}
+						}
+
+						if (obj.name.Contains(Terrain))
+						{
+							if (yeets.Contains(obj.name))
+							{
+								yield return null;
+								continue;
+							}
+
+							var components = obj.GetComponentsInChildren<FloorTypeObj>();
+							yeets.Add(obj.name);
+							yield return null;
+
+							for (int j = 0; j < components.Length; j++)
+							{
+								var component = components[j];
+								if (component != null)
+								{
+									SuisHackMain.loggerInst.Msg($"Wrote down {component.name}");
+									Directory.CreateDirectory("Squares");
+
+									var path = Path.Combine("Squares", component.name + ".xml");
+									if (File.Exists(path))
+										continue;
+
+									var mf = component.GetComponent<MeshFilter>();
+									if (mf == null)
+									{
+										SuisHackMain.loggerInst.Msg($"Mesh Filter was null in {component.name}");
+										continue;
+									}
+
+									if (mf.sharedMesh == null)
+									{
+										SuisHackMain.loggerInst.Msg($"Mesh was null in {component.name}");
+										continue;
+									}
+
+									var verts = mf.sharedMesh.vertices;
+									var triangles = mf.sharedMesh.triangles;
+									var uvs = mf.sharedMesh.uv;
+
+									var store = new Store();
+									store.verts = new Vector3[verts.Length];
+									for (int l = 0; l < verts.Length; l++)
+									{
+										store.verts[l] = verts[l];
+									}
+
+									store.triangles = new int[triangles.Length];
+									for (int l = 0; l < triangles.Length; l++)
+									{
+										store.triangles[l] = triangles[l];
+									}
+
+									store.uvs = new Vector2[uvs.Length];
+									for (int l = 0; l < uvs.Length; l++)
+									{
+										store.uvs[l] = uvs[l];
+									}
+
+
+									var fileWritter = new StreamWriter(path);
+									XmlSerializer serializer = new XmlSerializer(typeof(Store));
+									serializer.Serialize(fileWritter, store);
+									fileWritter.Close();
+								}
+								yield return null;
+							}
+						}
+						yield return null;
+					}
+					else
+						yield return null;
+				}
+			}
+		}
+
+		private void OnDisable()
+		{
+			if (CheckerCoroutine != null)
+			{
+				MelonCoroutines.Stop(CheckerCoroutine);
+				CheckerCoroutine = null;
+			}
+		}
+	}
+
+	[Serializable]
+	public struct Store
+	{
+		public Vector3[] verts;
+		public int[] triangles;
+		public Vector2[] uvs;
+	}
+}
--- Source/Components/WireRendererCorrection.cs
+++ Source/Components/WireRendererCorrection.cs
@@ -1,5 +1,7 @@
 ﻿using MelonLoader;
 using System;
+using System.Collections.Generic;
+using UnhollowerBaseLib;
 using UnityEngine;
 
 namespace SuisHack.Components
@@ -7,11 +9,21 @@ namespace SuisHack.Components
 	[RegisterTypeInIl2Cpp]
 	public class WireRendererCorrection : MonoBehaviour
 	{
+		public static Dictionary<string, List<PoleDefinitionProtoplast>> DisableUtterlyBroken = new Dictionary<string, List<PoleDefinitionProtoplast>>()
+		{
+			{
+				"OpenWorld_J_09_Location_ElectricPole(Clone)", new List<PoleDefinitionProtoplast>()
+				{
+					new PoleDefinitionDisable("Pole Standard (9)", Vector3.zero),
+				}
+			}
+		};
+
 		public WireRendererCorrection(IntPtr ptr) : base(ptr) { }
 
 		public void OnEnable()
 		{
-			var skinnedMeshes = GetComponentsInChildren<SkinnedMeshRenderer>(true);
+			Il2CppArrayBase<SkinnedMeshRenderer> skinnedMeshes = GetComponentsInChildren<SkinnedMeshRenderer>(true);
 			for (int i = 0; i < skinnedMeshes.Length; i++)
 			{
 				var newBound = new Bounds();
@@ -31,6 +43,86 @@ namespace SuisHack.Components
 
 				skinnedMeshes[i].localBounds = newBound;
 			}
+
+			if (transform.parent != null)
+			{
+				if (DisableUtterlyBroken.TryGetValue(transform.parent.name, out var poles))
+				{
+					foreach (var pole in poles)
+					{
+						if (pole.Name == this.transform.name)
+						{
+							pole.Process(skinnedMeshes);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public abstract class PoleDefinitionProtoplast
+	{
+		public string Name { get; private set; }
+
+		public PoleDefinitionProtoplast(string Name)
+		{
+			this.Name = Name;
+		}
+
+		public static bool operator ==(PoleDefinitionProtoplast p, Transform transform)
+		{
+			return p.IsValidCompare(transform);
+		}
+
+		public static bool operator !=(PoleDefinitionProtoplast p, Transform transform)
+		{
+			return !p.IsValidCompare(transform);
+		}
+
+		protected abstract bool IsValidCompare(Transform transform);
+		public abstract void Process(UnhollowerBaseLib.Il2CppArrayBase<SkinnedMeshRenderer> skinnedMeshes);
+	}
+
+	public class PoleDefinitionDisable : PoleDefinitionProtoplast
+	{
+		public Vector3 LocalEulerRotation { get; private set; }
+
+		public PoleDefinitionDisable(string Name, Vector3 LocalEulerRotation) : base(Name)
+		{
+			this.LocalEulerRotation = LocalEulerRotation;
+		}
+
+		protected override bool IsValidCompare(Transform transform)
+		{
+			return this.LocalEulerRotation == LocalEulerRotation && this.Name == transform.name;
+		}
+
+		public override void Process(Il2CppArrayBase<SkinnedMeshRenderer> skinnedMeshes)
+		{
+			if(skinnedMeshes.Length > 0)
+			{
+				skinnedMeshes[0].transform.parent.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	public class PoleDefinitionCorrection : PoleDefinitionProtoplast
+	{
+		public Vector3 LocalEulerRotation { get; private set; }
+
+		public PoleDefinitionCorrection(string Name, Vector3 LocalEulerRotation) : base(Name)
+		{
+			this.LocalEulerRotation = LocalEulerRotation;
+		}
+
+		protected override bool IsValidCompare(Transform transform)
+		{
+			return this.LocalEulerRotation == LocalEulerRotation && this.Name == transform.name;
+		}
+
+		public override void Process(Il2CppArrayBase<SkinnedMeshRenderer> skinnedMeshes)
+		{
+			SuisHackMain.loggerInst.Msg("Implement that!");
 		}
 	}
 }
