bba62fed6626bc1ad87c7e2328417d13a4bb4474
SuiMachine <SuiMachine@users.noreply.github.com>
2022-08-21 23:08:49 +0200

    Override texture settings for roads to be filtered

--- Source/Components/GlobalGeometryChecker.cs
+++ Source/Components/GlobalGeometryChecker.cs
@@ -1,5 +1,6 @@
 ﻿using MelonLoader;
 using System;
+using System.Linq;
 using UnityEngine;
 
 namespace SuisHack.Components
@@ -17,6 +18,47 @@ namespace SuisHack.Components
 			if (CheckerCoroutine != null)
 				MelonCoroutines.Stop(CheckerCoroutine);
 			CheckerCoroutine = MelonCoroutines.Start(Checker());
+
+			var normalMapHash = Shader.PropertyToID("_BumpMap");
+			var metalicGlossMapHash = Shader.PropertyToID("_MetallicGlossMap");
+			var parallaxMapHash = Shader.PropertyToID("_ParallaxMap");
+			var occlusionMapHash = Shader.PropertyToID("_OcclusionMap");
+
+			var roadMesh = FindObjectsOfType<MeshRenderer>().Where(x => x.name == "Road_world").FirstOrDefault();
+
+			if(roadMesh != null)
+			{
+				for(int i=0; i< roadMesh.materials.Length; i++)
+				{
+					var material = roadMesh.materials[i];
+					if (material == null)
+						continue;
+
+					var mainTexture = material.mainTexture;
+					if (mainTexture != null)
+						mainTexture.filterMode = FilterMode.Trilinear;
+
+					var normalMap = material.GetTexture(normalMapHash);
+					if (normalMap != null)
+						normalMap.filterMode = FilterMode.Trilinear;
+
+					var metallicMap = material.GetTexture(metalicGlossMapHash);
+					if (metallicMap != null)
+						metallicMap.filterMode = FilterMode.Trilinear;
+
+					var parallaxMap = material.GetTexture(parallaxMapHash);
+					if (parallaxMap != null)
+						parallaxMap.filterMode = FilterMode.Trilinear;
+
+					var occlusionMap = material.GetTexture(occlusionMapHash);
+					if (occlusionMap != null)
+						occlusionMap.filterMode = FilterMode.Trilinear;
+				}
+			}
+			else
+			{
+				SuisHackMain.loggerInst.Error("No mesh found!");
+			}
 		}
 
 		private System.Collections.IEnumerator Checker()
