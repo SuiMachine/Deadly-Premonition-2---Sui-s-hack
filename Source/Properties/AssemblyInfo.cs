028b68455e71427c29688976433c2f9a872088ed
SuiMachine <SuiMachine@users.noreply.github.com>
2023-10-28 17:06:55 +0200

    Missing state support tracking

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using MelonLoader;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("d8f834c8-2b43-49f6-88f5-afe5873b8972")]
 
 [assembly: MelonInfo(typeof(SuisHack.SuisHackMain), "SuisHack", "1.1.0", "SuicideMachine", "https://github.com/SuiMachine/Deadly-Premonition-2---Sui-s-hack/releases")]
 [assembly: MelonGame("ToyBox Inc.", "DeadlyPremonition2")]
